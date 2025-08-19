57c5bd81d50019d61c739d2f818588664664fadd
seta-minhnguyentuan <minh.nguyentuan@seta-international.vn>
2025-08-19 15:32:58 +0700

    refactor: Simplify migration logic by removing redundant SQL commands and streamline extension creation

--- user-service/internal/database/migration.go
+++ user-service/internal/database/migration.go
@@ -7,49 +7,9 @@ import (
 )
 
 func Migrate(db *gorm.DB) error {
-	sqlDB, err := db.DB()
-	if err != nil {
+	if err := db.Exec(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`).Error; err != nil {
 		return err
 	}
 
-	_, err = sqlDB.Exec("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";")
-	if err != nil {
-		return err
-	}
-
-	err = db.AutoMigrate(&models.User{}, &models.Team{}, &models.TeamMember{})
-	if err != nil {
-		return err
-	}
-
-	_, err = sqlDB.Exec(`
-		DO $$ 
-		BEGIN
-			-- Add team_id foreign key constraint if it doesn't exist
-			IF NOT EXISTS (
-				SELECT 1 FROM information_schema.table_constraints 
-				WHERE constraint_name = 'fk_team_members_team_id'
-			) THEN
-				ALTER TABLE team_members 
-				ADD CONSTRAINT fk_team_members_team_id 
-				FOREIGN KEY (team_id) REFERENCES teams(id) ON DELETE CASCADE;
-			END IF;
-
-			-- Add user_id foreign key constraint if it doesn't exist
-			IF NOT EXISTS (
-				SELECT 1 FROM information_schema.table_constraints 
-				WHERE constraint_name = 'fk_team_members_user_id'
-			) THEN
-				ALTER TABLE team_members 
-				ADD CONSTRAINT fk_team_members_user_id 
-				FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
-			END IF;
-		END $$;
-	`)
-	if err != nil {
-		// Log warning but don't fail the migration
-		return nil
-	}
-
-	return nil
+	return db.AutoMigrate(&models.User{}, &models.Team{}, &models.TeamMember{})
 }
